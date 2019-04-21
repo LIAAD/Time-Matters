5f13de1ea3a9f037fa20c04c85f21e90a21c7a14
JMendes1995 <35015528+JMendes1995@users.noreply.github.com>
2019-04-22 00:30:25 +0100

    Update time_matters.py

--- time_matters/time_matters.py
+++ time_matters/time_matters.py
@@ -1,11 +1,9 @@
 from time_matters.dictionary import kw_ext
 from time_matters.dataframes import dt_frames
 from langdetect import detect
-import nltk
 
 def timeMatters(txt, limit_distance=5, threshold=0.05, max_array_len=0):
     #detect language of the text
-    nltk.download('punkt')
     lang = detect(txt)
     dictionary, words_array, dates_array = kw_ext(lang, txt)
     relevant_dates = dt_frames(dictionary, words_array, dates_array, limit_distance, threshold, max_array_len)
