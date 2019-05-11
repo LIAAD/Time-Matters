9036a9ee19dce4d986bbedf0ef5aa4cdbedef46a
JMendes1995 <35015528+JMendes1995@users.noreply.github.com>
2019-05-11 12:42:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(name='time_matters',
       url='https://github.com/JMendes1995/Time_Matters.git',
       packages=find_packages(include=['time_matters', 'nltk', 'pandas', 'langdetect', 'tabulate', 'click']),
       py_modules=['time_matters'],
-      install_requires=["yake==0.4.1", "py_heudeltime"],
-      dependency_links=['https://github.com/LIAAD/yake.git#egg=yake-0.4.1','https://github.com/JMendes1995/py_heideltime.git#egg=pakage']
+      install_requires=["yake==0.3.7", "py_heudeltime"],
+      dependency_links=['https://github.com/LIAAD/yake.git#egg=yake-0.3.7','https://github.com/JMendes1995/py_heideltime.git#egg=pakage']
 )
 
 
