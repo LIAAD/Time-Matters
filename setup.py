45453903977535b96672ea8e4353a6957bd09ea4
JMendes1995 <35015528+JMendes1995@users.noreply.github.com>
2019-05-12 01:25:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
-setup(name='time_matters',
-      version='1.2',
-      description='module that discover the relevant dates from a text',
-      author='Jorge Alexandre Rocha Mendes',
-      author_email='mendesjorge49@gmail.com',
-      url='https://github.com/JMendes1995/Time_Matters.git',
-      packages=find_packages(include=['time_matters', 'nltk', 'pandas', 'langdetect', 'tabulate', 'click']),
-      py_modules=['time_matters']
-)
 
+def exexnltk():
+      import nltk
+      nltk.download('punkt')
+
+if __name__ == '__main__':
+      exexnltk()
+      setup(name='time_matters',
+            version='1.2',
+            description='module that discover the relevant dates from a text',
+            author='Jorge Alexandre Rocha Mendes',
+            author_email='mendesjorge49@gmail.com',
+            url='https://github.com/JMendes1995/Time_Matters.git',
+            packages=find_packages(include=['time_matters', 'nltk', 'pandas', 'langdetect', 'tabulate', 'click']),
+            py_modules=['time_matters']
+      )
 
 
