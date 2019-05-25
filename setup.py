f6ccbda7b393681eb0fec85bfa5c1250b3ac2566
Jorge Mendes <mendesjorge49@gmail.com>
2019-05-25 01:21:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,20 +13,12 @@ install_requires = [item.strip() for item in requires if not "http" in item]
 dependency_links = [item.strip() for item in requires if "http" in item]
 
 
-class Install(_install):
-    def run(self):
-        _install.do_egg_install(self)
-        import nltk
-        nltk.download("punkt")
-
-
 setup(name='time_matters',
       version='1.2',
       description='module that discover the relevant dates from a text',
       author='Jorge Alexandre Rocha Mendes',
       author_email='mendesjorge49@gmail.com',
       url='https://github.com/JMendes1995/Time_Matters.git',
-      cmdclass={'install': Install},
       packages=find_packages(),
       dependency_links=dependency_links,
       entry_points={
