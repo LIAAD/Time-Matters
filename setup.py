6f82d98451416ef57dc14ce5097d64b767c1261b
JMendes1995 <35015528+JMendes1995@users.noreply.github.com>
2019-05-26 08:54:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
 from setuptools import setup, find_packages
 from setuptools.command.install import install as _install
 
-import os
-requirementPath = 'requirements.txt'
-install_requires = []
 
-if os.path.isfile(requirementPath):
-    with open('requirements.txt') as f:
-        requires = f.readlines()
-
-install_requires = [item.strip() for item in requires if not "http" in item]
-dependency_links = [item.strip() for item in requires if "http" in item]
-
+class Install(_install):
+    def run(self):
+        _install.do_egg_install(self)
+        import nltk
+        nltk.download("punkt")
 
 setup(name='time_matters',
       version='1.2',
@@ -20,7 +15,10 @@ setup(name='time_matters',
       author_email='mendesjorge49@gmail.com',
       url='https://github.com/JMendes1995/Time_Matters.git',
       packages=find_packages(),
-      dependency_links=dependency_links,
+      include_package_data=True,
+      cmdclass={'install': Install},
+      dependency_links=['git+https://github.com/LIAAD/yake',
+                        'git+https://github.com/JMendes1995/py_heideltime'],
       entry_points={
           'console_scripts': [
               'time_matters=time_matters.cli:Dates'
