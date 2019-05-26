52fe2b20af2386ca915e7a1eaa5bb23f5932632f
JMendes1995 <35015528+JMendes1995@users.noreply.github.com>
2019-05-26 10:03:16 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,6 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install as _install
 
 
-class Install(_install):
-    def run(self):
-        _install.do_egg_install(self)
-        import nltk
-        nltk.download("punkt")
-
 setup(name='time_matters',
       version='1.2',
       description='module that discover the relevant dates from a text',
@@ -16,7 +10,6 @@ setup(name='time_matters',
       url='https://github.com/JMendes1995/Time_Matters.git',
       packages=find_packages(),
       include_package_data=True,
-      cmdclass={'install': Install},
       entry_points={
           'console_scripts': [
               'time_matters=time_matters.cli:Dates'
