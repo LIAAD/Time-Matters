a096a9429bb17f8dd22d35dc1944116c25274666
JMendes1995 <35015528+JMendes1995@users.noreply.github.com>
2019-05-27 13:30:02 +0100

    Update cli.py

--- time_matters/cli.py
+++ time_matters/cli.py
@@ -1,8 +1,8 @@
 import click
 from time_matters import timeMatters, timeMattersPerSentence
 @click.command()
-@click.option("-t", '--text', help='insert text', required=False)
-@click.option("-l", '--language', help='insert language', required=True)
+@click.option("-t", '--text', help='insert text, text should be surrounded by quotes “” (e.g., “Thurs August 31st”)', required=False)
+@click.option("-l", '--language', help='[required] Language text is required and should be surrounded by quotes “”. Options: English, Portuguese, Spanish, Germany, Dutch, Italian, French (e.g., “English”).', required=True)
 @click.option("-dps", '--date_per_sentence', help='select if want to analyze per sentence',default=False ,required=False)
 @click.option("-cwd", '--context_window_distance', help='max distance between words',default=10, required=False)
 @click.option("-th", '--threshold', help='minimum DICE threshold similarity values',default=0.05, required=False)
@@ -10,9 +10,9 @@ from time_matters import timeMatters, timeMattersPerSentence
 @click.option("-ky", '--max_keywords', help='max keywords',default=10 ,required=False)
 @click.option("-icwd", '--ignore_contextual_window_distance', help='ignore contextual window distance',default=False ,required=False)
 @click.option("-aps", '--analysis_sentence', help='DICE Calculation per sentence',default=True ,required=False)
-@click.option("-td", '--heideltime_document_type', help='Type of the document specified by <file> (options: News, Narrative, Colloquial, Scientific).', default='News', required=False)
-@click.option("-dct", '--heideltime_document_creation_time', help='Creation date of document only valid format (YYYY-MM-DD).only will be considered if document type are News or colloquial.', default="", required=False)
-@click.option("-i", '--input_file', help='input text file', required=False)
+@click.option("-dt", '--heideltime_document_type', help='Type of the document text should be surrounded by quotes “”. Options: News, Narrative, Colloquial, Scientific (e.g., “News”).', default='News', required=False)
+@click.option("-dct", '--heideltime_document_creation_time', help=' Document creation date in the format YYYY-MM-DD should be surrounded by quotes (e.g., “2019-05-30”). Note that this date will only be taken into account when News or Colloquial texts are specified.', default="", required=False)
+@click.option("-i", '--input_file', help=' text path should be surrounded by quotes (e.g., “text.txt”)', required=False)
 def Dates(text, language, date_per_sentence, context_window_distance, threshold, max_array_len, max_keywords, ignore_contextual_window_distance, analysis_sentence, input_file, heideltime_document_type, heideltime_document_creation_time):
     def run_time_matters(text_content):
         if date_per_sentence:
