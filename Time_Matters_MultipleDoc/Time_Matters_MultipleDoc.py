47b71df7015a34009359bf513e8734cfa0364d6c
JMendes1995 <35015528+JMendes1995@users.noreply.github.com>
2019-06-01 09:01:42 +0100

    Update Time_Matters_MultipleDoc.py

--- Time_Matters_MultipleDoc/Time_Matters_MultipleDoc.py
+++ Time_Matters_MultipleDoc/Time_Matters_MultipleDoc.py
@@ -29,10 +29,3 @@ def get_final_output_doc(dictionary, list_dates_score, sentence_index):
         final_output.append((lt[0], [(sentence_index, lt[1], total_offset)]))
     return final_output
 
-
-if __name__ == '__main__':
-    list_of_docs = ['''The Carnation Revolution (Portuguese: Revolução dos Cravos), also known as the 25th of April (Portuguese: 25 de Abril), was initially a 25 April 1974 military coup in Lisbon which overthrew the authoritarian Estado Novo regime.[1] The revolution began as a coup organised by the Armed Forces Movement (Portuguese: Movimento das Forças Armadas, MFA), composed of military officers who opposed the regime, but it was soon coupled with an unanticipated, popular civil resistance campaign. The revolution led to the fall of the Estado Novo, the end of 48 years of authoritarian rule in Portugal, and Portugal's withdrawal from its African colonies.''',
-                    '''Thurs August 31st - News today that they are beginning to evacuate the London children tomorrow. Percy is a billeting officer. I can't see that they will be much safer here.''']
-
-    print(Time_Matters_MultipleDoc(list_of_docs, language="English"))
-
