9986619dc7b55243c093dda50fafbebc85452c15
JMendes1995 <mendesjorge49@gmail.com>
2019-07-08 22:19:28 +0100

    Update validate_input.py

--- Time_Matters_SingleDoc/validate_input.py
+++ Time_Matters_SingleDoc/validate_input.py
@@ -1,18 +1,18 @@
 def verify_time_matters(num_of_keywords, N, n_contextual_window, TH):
-    if not isinstance(num_of_keywords, int):
+    if not isinstance(num_of_keywords, int) and num_of_keywords < 0:
         print('Please specify a valid num_of_keywords'
               'options:\n'
               'n, where n is any integer > 0;')
         return {}
 
-    elif n_contextual_window != 'full_sentence' and not isinstance(n_contextual_window, int) and n_contextual_window < 0:
+    elif n_contextual_window != 'full_sentence' or (not isinstance(n_contextual_window, int) and n_contextual_window < 0):
         print('Please specify a valid n_contextual_window\n'
               'options:\n'
               '     full_sentence;\n'
               '     n, where n is any integer > 0;')
         return {}
 
-    elif N != 'max' and not isinstance(N, int) and N < 0:
+    elif N != 'max' or (isinstance(N, int) and N < 0):
         print('Please specify a valid n context vector size\n'
               'options:\n'
               '     max;\n'
@@ -22,7 +22,7 @@ def verify_time_matters(num_of_keywords, N, n_contextual_window, TH):
     elif not isinstance(TH, float) and TH < 0:
         print('Please specify a valid TH threshold\n'
               'options:\n'
-              '     number(float);')
+              '      n, where n is any float > 0;')
         return {}
 
 
